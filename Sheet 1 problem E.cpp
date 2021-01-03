7716250eaa51a6fa648ce4efbdfc266aaa8b57bc
Riaz Ahmed Mahin <71597653+riazahmedmahin@users.noreply.github.com>
2021-01-03 18:13:48 +0600

    Sheet 1 problem E.cpp

---
+++ Sheet 1 problem E.cpp
@@ -0,0 +1,14 @@
+// prepared by @Mahin
+#include<iostream>
+#include<iomanip>
+using namespace std;
+int main()
+{
+    double a,b,x=3.141592653;
+    cin>>a;
+    b=x*a*a;
+    cout<<fixed<<setprecision(9)<<b<<endl;
+
+    
+
+}
