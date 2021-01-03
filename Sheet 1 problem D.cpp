775c26fdae483688e9a47ef3f9bbb1ece6a8a787
Riaz Ahmed Mahin <71597653+riazahmedmahin@users.noreply.github.com>
2021-01-03 18:12:16 +0600

    Sheet 1 problem D.cpp

---
+++ Sheet 1 problem D.cpp
@@ -0,0 +1,12 @@
+//prepared by @Mahin
+#include<bits/stdc++.h>
+using namespace std;
+int main()
+{
+    long long  int a,b,c,d,e;
+    cin>>a>>b>>c>>d;
+    e=(a*b)-(c*d);
+    cout<<"Difference = "<<e<<endl;
+    
+
+}
