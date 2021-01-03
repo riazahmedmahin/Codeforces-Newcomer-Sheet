d9936b1ed0e0e8bd0df905b1b7fd6a22531ab722
Riaz Ahmed Mahin <71597653+riazahmedmahin@users.noreply.github.com>
2021-01-03 18:21:35 +0600

    Create Sheet 1 problem I.cpp

---
+++ Sheet 1 problem I.cpp
@@ -0,0 +1,16 @@
+//prepared by @Mahin
+#include<iostream>
+using namespace std;
+int main()
+{
+    int a,b;
+    cin>>a>>b;
+    if(a>b|| a==b ){
+        cout<<"Yes";
+    }
+    else
+    {
+        cout<<"No";
+    } 
+
+}
