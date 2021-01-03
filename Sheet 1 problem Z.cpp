f943fd7d0368f0f35ba56fce237c04b6404cf943
Riaz Ahmed Mahin <71597653+riazahmedmahin@users.noreply.github.com>
2021-01-03 18:41:31 +0600

    Create Sheet 1 problem Z.cpp

---
+++ Sheet 1 problem Z.cpp
@@ -0,0 +1,17 @@
+//prepared by @Mahin
+#include<bits/stdc++.h>
+using namespace std;
+int main()
+{
+long long int a,b,c,d;
+cin>>a>>b>>c>>d;
+if(b*log(a) > d*log(c))
+{
+    cout<<"YES";
+}
+else
+{
+    cout<<"NO";
+}
+
+}
