ff15808d1b000956584fe6f6190d1f4ba3ada9c0
Riaz Ahmed Mahin <71597653+riazahmedmahin@users.noreply.github.com>
2021-01-03 18:28:25 +0600

    Create Sheet 1 problem N.c

---
+++ Sheet 1 problem N.c
@@ -0,0 +1,20 @@
+//prepared by @Mahin
+#include<stdio.h>
+int main()
+{
+    char ch;
+    scanf("%c",&ch);
+   
+        if(ch>=65 && ch<=90)
+        {
+            printf("%c",ch+32);
+        }
+        else
+        {
+            if (ch>=97 && ch<=122)
+            {
+                printf("%c",ch-32);
+            }
+           
+        }
+    }
