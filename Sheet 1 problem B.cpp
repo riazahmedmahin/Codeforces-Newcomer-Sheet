dcbbd038936d11428f7db6bd096bf13be02be131
Riaz Ahmed Mahin <71597653+riazahmedmahin@users.noreply.github.com>
2021-01-03 17:58:42 +0600

    Sheet 1 problem B.cpp

---
+++ Sheet 1 problem B.cpp
@@ -0,0 +1,17 @@
+//prepared by @Mahin
+#include<iostream>
+using namespace std;
+int main()
+{
+    int a;
+    long long int b;
+    char c;
+    float d;
+    double f;
+    cin >>a>>b>>c>>d>>f;
+    cout<<a<<endl;
+    cout<<b<<endl;
+    cout<<c<<endl;
+    cout<<d<<endl;
+    cout<<f<<endl;
+}
