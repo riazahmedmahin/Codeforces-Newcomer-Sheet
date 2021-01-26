b8a97c2f0840638aef77a8f29c15383f520b322e
Riaz Ahmed Mahin <71597653+riazahmedmahin@users.noreply.github.com>
2021-01-26 21:04:12 +0600

    Create Sheet 1 problem V.cpp

---
+++ Sheet 1 problem V.cpp
@@ -0,0 +1,37 @@
+// prepain by @ Mahin
+
+#include<iostream>
+using namespace std;
+
+int main()
+{
+    int num1, num2;
+    char ch;
+    cin>>num1>>ch>>num2;
+
+    if(ch == '<')
+    {
+
+        if(num1 < num2)
+           cout<<"Right\n";
+        else
+            cout<<"Wrong\n";
+    }
+    else  if(ch == '>')//
+    {
+
+        if(num1 > num2)
+            cout<<"Right\n";
+        else
+             cout<<"Wrong\n";
+    }
+    else
+    {
+        if (num1 == num2)
+            cout<<"Right\n";
+        else
+            cout<<"Wrong\n";
+    }
+    return 0;
+}
+
