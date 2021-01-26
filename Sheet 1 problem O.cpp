bdf44d0234a08a61dbcfde7adb73c5e02778c9eb
Riaz Ahmed Mahin <71597653+riazahmedmahin@users.noreply.github.com>
2021-01-26 20:59:29 +0600

    Create Sheet 1 problem O.cpp

---
+++ Sheet 1 problem O.cpp
@@ -0,0 +1,36 @@
+// prepaid by @ Mahin
+
+#include<iostream>
+using namespace std;
+#include<math.h>
+int main()
+{
+    int a, b, sum;
+    char ch = 's';
+
+    cin>>a>>ch>>b;
+    if((a>=1 && a<=pow(10,4)) && (b>=1 && b<= pow(10,4)))
+    {
+        if(ch == '+')
+        {
+            sum = a + b;
+            cout<<sum<<endl;
+        }
+        else if(ch == '-')
+        {
+            sum = a - b;
+            cout<<sum<<endl;
+        }
+        else if(ch == '*')
+        {
+            sum = a * b;
+            cout<<sum<<endl;
+        }
+        else if(ch == '/')
+        {
+            sum = a / b;
+            cout<<sum<<endl;
+        }
+    }
+    return 0;
+}
