4f19e3284c153ceed74a33b93397263d1631fb2a
Riaz Ahmed Mahin <71597653+riazahmedmahin@users.noreply.github.com>
2021-01-27 18:12:56 +0600

    Create Sheet 2 problem B.cpp

---
+++ Sheet 2 problem B.cpp
@@ -0,0 +1,19 @@
+// prepared by @ mahin 
+#include<iostream>
+using namespace  std;
+
+int main()
+{
+int n,i,count=0;
+cin>>n;
+for(i=1;i<=n;i++){
+if(i%2==0){
+cout<<i<<endl;
+count++;
+}
+}
+if(count++==0)
+cout<<"-1";
+
+return 0;
+}
