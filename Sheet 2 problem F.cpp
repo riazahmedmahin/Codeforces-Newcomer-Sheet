1e6e02d71784480065ad096958db96d9ae1f21f1
Riaz Ahmed Mahin <71597653+riazahmedmahin@users.noreply.github.com>
2021-01-27 18:24:02 +0600

    Create Sheet 2 problem F.cpp

---
+++ Sheet 2 problem F.cpp
@@ -0,0 +1,14 @@
+// prepared by @ Mahin
+
+#include<iostream>
+using namespace std;
+int main()
+{
+    int a,i;
+    cin>>a;
+    for(i=1;i<=12;i++)
+    cout<< a <<" * " << i << " = " << a*i << endl;
+
+
+
+}
