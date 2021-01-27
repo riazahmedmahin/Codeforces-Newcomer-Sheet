24d86bb6ffdf9545eee41f96b25e5347ca0337f8
Riaz Ahmed Mahin <71597653+riazahmedmahin@users.noreply.github.com>
2021-01-27 18:40:27 +0600

    Create Sheet 2 problem P.cpp

---
+++ Sheet 2 problem P.cpp
@@ -0,0 +1,22 @@
+//prepared by @ Mahin
+
+#include <iostream>
+using namespace std;
+
+int main()
+{
+    int rows;
+
+  
+    cin >> rows;
+
+    for(int i = rows; i >= 1; i--)
+    {
+        for(int j = 1; j <= i; ++j)
+        {
+            cout << "*";
+        }
+        cout << "\n";
+    }
+    return 0;
+}
