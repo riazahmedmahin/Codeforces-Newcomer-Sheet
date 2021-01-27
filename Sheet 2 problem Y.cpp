4eadd34ff6b8838008ecf1f494cb391c73d1ccfa
Riaz Ahmed Mahin <71597653+riazahmedmahin@users.noreply.github.com>
2021-01-27 19:04:39 +0600

    Create Sheet 2 problem Y.cpp

---
+++ Sheet 2 problem Y.cpp
@@ -0,0 +1,24 @@
+// prepared by @ Mahin
+
+
+#include<iostream>
+using namespace std;
+int main()
+{
+    int first=0,second=1,count=0,fibo,n;
+
+    cin>>n;
+    while(count<n){
+        if(count<=1){
+            fibo=count;
+        }
+        else{
+            fibo=first+second;
+            first=second;
+            second=fibo;
+        }
+        cout<<fibo<<" ";
+        count++;
+    }
+
+}
