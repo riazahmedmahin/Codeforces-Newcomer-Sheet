901eac223358ed5a0be129ff46f3f43f4ba0a5ac
Riaz Ahmed Mahin <71597653+riazahmedmahin@users.noreply.github.com>
2021-01-28 23:23:10 +0600

    Create Sheet 1 problem U.cpp

---
+++ Sheet 1 problem U.cpp
@@ -0,0 +1,20 @@
+// prepated by @ Mahin
+
+
+#include <iostream>
+using namespace std;
+
+int main() {
+  
+  double a;
+  cin >> a;
+  
+  int b=a;
+  
+  double x=a-b;
+  
+  if(x==0) cout << "int " << b;
+  else cout << "float " << b << " " << x;
+  
+  
+}
