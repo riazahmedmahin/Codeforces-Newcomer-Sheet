edd6079297183640f7c2f8349a092c433d83871e
Riaz Ahmed Mahin <71597653+riazahmedmahin@users.noreply.github.com>
2021-03-09 10:07:29 +0600

    Create Sheet 3 problem H.cpp

---
+++ Sheet 3 problem H.cpp
@@ -0,0 +1,25 @@
+// Prepared by @Mahin
+
+#include<iostream>
+using namespace std;
+int main () 
+{
+  int n, i, j, temp;
+  cin>>n;
+  int a[n];
+  for(i=0;i<n;i++)
+    cin>>a[i];
+  for(i=0;i<n;i++)
+    {
+     for(j=0;j<(n-i-1);j++)
+	  if(a[j]>a[j+1])
+	  {
+	    temp=a[j];
+	    a[j]=a[j+1];
+	    a[j+1]=temp;
+	  }
+    }
+   for(i=0;i<n;i++)
+    cout<<a[i]<<" ";
+  return 0;
+}
