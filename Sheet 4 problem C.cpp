0ae9ec2e56dddc741b37dd9f1f94d235b9066f5f
Riaz Ahmed Mahin <71597653+riazahmedmahin@users.noreply.github.com>
2021-03-23 18:54:48 +0600

    Create Sheet 4 problem C.cpp

---
+++ Sheet 4 problem C.cpp
@@ -0,0 +1,16 @@
+// by Mahin 
+ #include<iostream>
+using namespace std;
+int main()
+{
+
+    string s1,s2;
+    cin>>s1>>s2;
+     if(s1<s2)
+        cout<<s1;
+     else
+        cout<<s2;
+
+
+
+}
