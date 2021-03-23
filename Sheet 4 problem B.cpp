749d6327c21cb65f5b218f77a7909c3671bc76da
Riaz Ahmed Mahin <71597653+riazahmedmahin@users.noreply.github.com>
2021-03-23 19:20:04 +0600

    Create Sheet 4 problem B.cpp

---
+++ Sheet 4 problem B.cpp
@@ -0,0 +1,15 @@
+// by @ Maghin 
+ #include<bits/stdc++.h>
+using namespace std;
+int main(){
+    char s[1000000];
+    gets(s);
+
+    for(long long int i=0;i<1000000;i++){
+        if(s[i]==92)
+        break;
+        cout<<s[i];
+    }
+
+    return 0;
+}
