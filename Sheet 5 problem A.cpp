4094a8d5b90be2196775984ddf229d1b0644cf5c
Riaz Ahmed Mahin <71597653+riazahmedmahin@users.noreply.github.com>
2021-04-06 20:55:19 +0600

    Create Sheet 5 problem A.cpp

---
+++ Sheet 5 problem A.cpp
@@ -0,0 +1,16 @@
+// by @Mahin
+
+#include<iostream>
+using namespace std;
+void sum( int a ,int b)
+{
+    int c=a+b;
+    cout<<c;
+}
+
+int main()
+{
+    int a,b;
+    cin>>a>>b;
+    sum(a,b);
+}
