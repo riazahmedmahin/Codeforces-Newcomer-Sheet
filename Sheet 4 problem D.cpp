a710767fb6c8fd1f9102542f4c5adebf17d4b93c
Riaz Ahmed Mahin <71597653+riazahmedmahin@users.noreply.github.com>
2021-04-07 20:14:47 +0600

    Update and rename Sheet 4 problem D.c to Sheet 4 problem D.cpp

--- Sheet 4 problem D.cpp
+++ Sheet 4 problem D.cpp
@@ -1,11 +1,11 @@
 // by @ mahin
 
-#include<stdio.h>
-#include<stdio.h>
+#include<bits/stdc++.h>
+using namespace std;
 
 int main(){
     char s[1000],t[1000];
-    scanf("%s %s",s,t);
+    cin>>s>>t;
     char x=s[0],y=t[0];
     int i,sl,tl;
 
@@ -13,16 +13,16 @@ int main(){
     tl=strlen(t);
 
     strcat(s,t);
-    printf("%d %d\n",sl,tl);
-    printf("%s\n",s);
+    cout<<sl<<" "<<tl<<endl;
+    cout<<s<<endl;
     for(i=0;i<sl;i++){
         s[0]=y;
-        printf("%c",s[i]);
+        cout<<s[i];
     }
-    printf(" ");
+    cout<<" ";
     for(i=0;i<tl;i++){
         t[0]=x;
-        printf("%c",t[i]);
+        cout<<t[i];
     }
 
     return 0;
