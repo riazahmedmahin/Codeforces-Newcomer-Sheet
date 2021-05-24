875937df7780cdacf959cf238beba2a49efb0a68
Riaz Ahmed Mahin <71597653+riazahmedmahin@users.noreply.github.com>
2021-05-24 21:20:33 +0600

    Update Sheet 5 problem L.cpp

--- Sheet 5 problem L.cpp
+++ Sheet 5 problem L.cpp
@@ -2,18 +2,28 @@
 
 #include<bits/stdc++.h>
 using namespace std;
-int main ()
+ 
+int NewArr(int a[], int b[], int n)
 {
+    for(int i=0; i<n; i++)
+        cout << b[i] << " ";
+    for(int i=0; i<n; i++)
+        cout << a[i] << " ";
 
-  int n;
-  cin >> n;
-  int arr[n], arr2[n], i;
-  for (i = 0; i < n; i++)
-    cin >> arr[i];
-  for (i = 0; i < n; i++)
-    cin >> arr2[i];
-  for (i = 0; i < n; i++)
-    cout << arr2[i] << " ";
-  for (i = 0; i < n; i++)
-    cout << arr[i] << " ";
+}
+ 
+int main()
+{
+    int n;
+    cin >> n;
+    int a[n],b[n];
+    for(int i=0; i<n; i++)
+    {
+        cin >> a[i];
+    }
+    for(int i=0; i<n; i++)
+    {
+        cin >> b[i];
+    }
+    NewArr(a,b,n);
 }
