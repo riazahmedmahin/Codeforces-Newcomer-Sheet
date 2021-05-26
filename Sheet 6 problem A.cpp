113b8364e7ecf07552fa3904c3cfef3ec77a6110
Riaz Ahmed Mahin <71597653+riazahmedmahin@users.noreply.github.com>
2021-05-26 21:26:44 +0600

    Create Sheet 6 problem A.cpp

---
+++ Sheet 6 problem A.cpp
@@ -0,0 +1,14 @@
+// by @ Mahin
+
+#include <bits/stdc++.h>
+using namespace std;
+#define ll long long
+int main ()
+{
+
+
+
+  ll n;
+  cin >> n;
+  ((ceil (log2 (n)) == floor (log2 (n)))) ? cout << "YES" : cout << "NO";
+}
